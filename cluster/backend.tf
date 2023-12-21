d8de9056af8ce97e827f4297d92505492816886c
srikredd <115576685+srikredd@users.noreply.github.com>
2023-12-21 15:49:25 +0000

    Name updated

--- cluster/backend.tf
+++ cluster/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
     bucket = "terraformaws97"
-    key    = "eks-prod.tfstate"
+    key    = "eks-prod1.tfstate"
   }
 }
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,4 +1,4 @@
 variable "environment" {
   type = string
-  default = "prod"
+  default = "prod1"
 }
